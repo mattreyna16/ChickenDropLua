7145f33716122a37c91d0f92b73bbd0e8a66903f
mattreyna16 <mattreyna16@gmail.com>
2026-05-06 23:06:37 -0500

    Adding pickup and switching chickenhead for bubble

--- main.lua
+++ main.lua
@@ -3,21 +3,24 @@
 --game ends when animal hits bottom
 
 function love.load()
-  chickenFace = love.graphics.newImage("chicken.png")
+  bubble = love.graphics.newImage("bubble.png")
+  pickup = love.graphics.newImage("pickup.png")
   backgroundImage = love.graphics.newImage("bg.png")
 
   math.randomseed(os.time())
   math.random(); math.random(); math.random()
-  startx = {math.random(0, love.graphics.getWidth() - chickenFace:getWidth()), 
-            math.random(0, love.graphics.getWidth() - chickenFace:getWidth()),  
-            math.random(0, love.graphics.getWidth() - chickenFace:getWidth()),  
-            math.random(0, love.graphics.getWidth() - chickenFace:getWidth()), 
-            math.random(0, love.graphics.getWidth() - chickenFace:getWidth())}
-  starty = {0 - math.random(chickenFace:getHeight(), chickenFace:getHeight() * 2),
-            0 - math.random(chickenFace:getHeight(), chickenFace:getHeight() * 2),
-            0 - math.random(chickenFace:getHeight(), chickenFace:getHeight() * 2),
-            0 - math.random(chickenFace:getHeight(), chickenFace:getHeight() * 2),
-            0 - math.random(chickenFace:getHeight(), chickenFace:getHeight() * 2)}
+  startx = {math.random(0, love.graphics.getWidth() - bubble:getWidth()), 
+            math.random(0, love.graphics.getWidth() - bubble:getWidth()),  
+            math.random(0, love.graphics.getWidth() - bubble:getWidth()),  
+            math.random(0, love.graphics.getWidth() - bubble:getWidth()), 
+            math.random(0, love.graphics.getWidth() - bubble:getWidth())}
+  starty = {0 - math.random(bubble:getHeight(), bubble:getHeight() * 2),
+            0 - math.random(bubble:getHeight(), bubble:getHeight() * 2),
+            0 - math.random(bubble:getHeight(), bubble:getHeight() * 2),
+            0 - math.random(bubble:getHeight(), bubble:getHeight() * 2),
+            0 - math.random(bubble:getHeight(), bubble:getHeight() * 2)}
+  pickupx = math.random(0, love.graphics.getWidth() - pickup:getWidth())
+  pickupy = 0 - math.random(pickup:getHeight())
 end
 
 -------------------------------------------------
@@ -28,17 +31,26 @@ function love.mousepressed(x, y, button, istouch)
   if button == 1 then
     --print("left mouse clicked")
     for i, v in ipairs(startx) do
-      --if the mouse x and y is within the boundary of a chicken picture
-      if x >= startx[i] and x <= startx[i] + chickenFace:getWidth() and y >= starty[i] and y <= starty[i] + chickenFace:getHeight() then
+      --if the mouse x and y is within the boundary of a bubble
+      if x >= startx[i] and x <= startx[i] + bubble:getWidth() and y >= starty[i] and y <= starty[i] + bubble:getHeight() then
         --print("in bounds")
         math.randomseed(os.time())
         math.random(); math.random(); math.random()
         --reset its y value (go back to the top)
-        starty[i] = math.random(chickenFace:getHeight(), chickenFace:getHeight() * 2) * -1
+        starty[i] = math.random(bubble:getHeight(), bubble:getHeight() * 2) * -1
+      end
+    end
+
+    --if the mouse x and y is within the boundary of a pickup
+    if x >= pickupx and x <= pickupx + pickup:getWidth() and y >= pickupy and y <= pickupy + pickup:getHeight() then
+      --print("in bounds")
+      math.randomseed(os.time())
+      math.random(); math.random(); math.random()
+      --reset its y value (go back to the top)
+      pickupy = math.random(pickup:getHeight(), pickup:getHeight() * 2) * -1
       end
     end
   end
-end
 
 -------------------------------------------------
 --UPDATE
@@ -46,13 +58,14 @@ end
 function love.update(dt)
   for i, v in ipairs(starty) do
     --if chicken hits the bottom of the screen, lua quits (we lose)
-    if starty[i] + chickenFace:getHeight() >= love.graphics.getHeight() then
+    if starty[i] + bubble:getHeight() >= love.graphics.getHeight() then
       --print("over the edge")
       love.event.quit()
     end
     --chickens move down 
     starty[i] = starty[i] + 80 * dt
   end
+  pickupy = pickupy + 80 * dt
 end
 
 -------------------------------------------------
@@ -62,6 +75,7 @@ function love.draw()
   love.graphics.draw(backgroundImage, 0, 0)
   --draw each chicken at their respective x and y
   for i, v in ipairs(startx) do
-    love.graphics.draw(chickenFace, startx[i], starty[i])
+    love.graphics.draw(bubble, startx[i], starty[i])
   end
+  love.graphics.draw(pickup, pickupx, pickupy)
 end
